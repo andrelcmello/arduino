cae84c8a3c160a2c08985e0f16da5cc9d608212f
andrelcmello <andrelcmello@gmail.com>
2017-02-03 22:03:02 -0200

    Set optimization level to 1 to make 'delay' module work. Improved main to actually blink the led.

--- Projects/blink_led/source/main.c
+++ Projects/blink_led/source/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <util/delay.h>
 #include <stdint.h>
 
 void main(void)
@@ -6,12 +7,19 @@ void main(void)
 	// Configure pin PB7 as output
 	DDRB |= 0x80;
 	
-	// Set PB7 High
-	PORTB |= 0x80;
-
-	// Set PB7 Low
-	PORTB &= ~0x80;
-	
 	// Infinite loop
-	while(1) {}
+	while(1) {
+
+		// Set PB7 High
+		PORTB |= 0x80;
+
+		// Wait 0.5 sec
+		_delay_ms(500);	
+
+		// Set PB7 Low
+		PORTB &= ~0x80;
+
+		// Wait 0.5 sec
+		_delay_ms(500);	
+	}
 }
